44567313ff1de7224b4ca10b1269d82ee218def4
mwiniarski <mw.pari@gmail.com>
2020-03-21 16:18:26 +0000

    Fix tabs to spaces

--- common/common.cpp
+++ common/common.cpp
@@ -3,9 +3,9 @@
 void initEAL(int &argc, char **argv[])
 {
     int ret = rte_eal_init(argc, *argv);
-	if (ret < 0)
-		rte_exit(EXIT_FAILURE, "ERROR: Can't init EAL\n");
+    if (ret < 0)
+        rte_exit(EXIT_FAILURE, "ERROR: Can't init EAL\n");
 
     argc -= ret;
-	argv += ret;
+    argv += ret;
 }
--- server/server.cpp
+++ server/server.cpp
@@ -32,46 +32,46 @@ using namespace std;
 
 int portInit(uint16_t port, rte_mempool *mbufPool)
 {
-	int ret;
-	uint16_t nb_rxd = ETH_RING_SIZE;
-	uint16_t nb_txd = ETH_RING_SIZE;
+    int ret;
+    uint16_t nb_rxd = ETH_RING_SIZE;
+    uint16_t nb_txd = ETH_RING_SIZE;
 
-	// Configure eth device
-	rte_eth_conf portConf;
-	memset(&portConf, 0, sizeof(rte_eth_conf));
-	portConf.rxmode.max_rx_pkt_len = ETHER_MAX_LEN;
+    // Configure eth device
+    rte_eth_conf portConf;
+    memset(&portConf, 0, sizeof(rte_eth_conf));
+    portConf.rxmode.max_rx_pkt_len = ETHER_MAX_LEN;
 
-	// Set 1 rx and 1 tx queue for given port
-	ret = rte_eth_dev_configure(port, 1, 1, &portConf);
-	if (ret != 0)
-		return ret;
+    // Set 1 rx and 1 tx queue for given port
+    ret = rte_eth_dev_configure(port, 1, 1, &portConf);
+    if (ret != 0)
+        return ret;
 
-	// Check if descriptor numbers are correct
-	ret = rte_eth_dev_adjust_nb_rx_tx_desc(port, &nb_rxd, &nb_txd);
-	if (ret != 0)
-		return ret;
+    // Check if descriptor numbers are correct
+    ret = rte_eth_dev_adjust_nb_rx_tx_desc(port, &nb_rxd, &nb_txd);
+    if (ret != 0)
+        return ret;
 
-	int sockId = rte_eth_dev_socket_id(port);
+    int sockId = rte_eth_dev_socket_id(port);
 
-	// Setup rx queue
-	ret = rte_eth_rx_queue_setup(port, 0, nb_rxd, sockId, NULL, mbufPool);
-	if (ret < 0)
-		return ret;
+    // Setup rx queue
+    ret = rte_eth_rx_queue_setup(port, 0, nb_rxd, sockId, NULL, mbufPool);
+    if (ret < 0)
+        return ret;
 
-	// Setup tx queue
-	ret = rte_eth_tx_queue_setup(port, 0, nb_txd, sockId, NULL);
-	if (ret < 0)
-		return ret;
+    // Setup tx queue
+    ret = rte_eth_tx_queue_setup(port, 0, nb_txd, sockId, NULL);
+    if (ret < 0)
+        return ret;
 
-	// Start eth device
-	ret = rte_eth_dev_start(port);
-	if (ret < 0)
-		return ret;
+    // Start eth device
+    ret = rte_eth_dev_start(port);
+    if (ret < 0)
+        return ret;
 
-	// Enable rx in promiscuous mode for eth device.
-	rte_eth_promiscuous_enable(port);
+    // Enable rx in promiscuous mode for eth device.
+    rte_eth_promiscuous_enable(port);
 
-	return 0;
+    return 0;
 }
 
 void initEth()
@@ -79,10 +79,10 @@ void initEth()
     // Check ports
     int portCount = rte_eth_dev_count();
 
-	if (portCount < 2)
-		rte_exit(EXIT_FAILURE, "ERROR: At least 2 ports needed, found %d\n", portCount);
+    if (portCount < 2)
+        rte_exit(EXIT_FAILURE, "ERROR: At least 2 ports needed, found %d\n", portCount);
 
-	cout << ">>> Found " << portCount << " ports available" << endl;
+    cout << ">>> Found " << portCount << " ports available" << endl;
 
     // Keep port info in system-wise accessible way
     const rte_memzone *memZone = rte_memzone_reserve(
@@ -106,7 +106,7 @@ void initEth()
                                        rte_socket_id());
 
     if (!mbufPool)
-			rte_exit(EXIT_FAILURE, "Error: Can't create mbuf pool\n");
+            rte_exit(EXIT_FAILURE, "Error: Can't create mbuf pool\n");
 
     cout << ">>> Mbuf pool " << MBUF_POOL_NAME << 
             " [" << MBUF_COUNT << "] created" << endl;
@@ -121,7 +121,7 @@ void initRings()
     sendRing = rte_ring_create(SEND_RING_NAME, SEND_RING_SIZE, rte_socket_id(), 0);
 
     if (!sendRing)
-		rte_exit(EXIT_FAILURE, "ERROR: Problem with creating send ring\n"); 
+        rte_exit(EXIT_FAILURE, "ERROR: Problem with creating send ring\n"); 
 
     cout << ">>> Ring " << SEND_RING_NAME << " [" 
          << SEND_RING_SIZE << "] created" << endl;
